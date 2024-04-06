c50d30791ef763a972cecb3ba72b3e7da4a90bfd
maltesermailo <jannik@andreasmueller.de>
2024-04-06 21:36:22 +0200

    Add stuff for mount points and file types

--- kernel/fs/vfs.h
+++ kernel/fs/vfs.h
@@ -7,6 +7,14 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#define FILE_TYPE_FILE 0x1
+#define FILE_TYPE_DIR 0x2
+#define FILE_TYPE_BLOCK_DEVICE 0x3
+#define FILE_TYPE_VIRTUAL_DEVICE 0x4
+#define FILE_TYPE_MOUNT_POINT 0x5
+#define FILE_TYPE_DEVICES 0x6
+#define FILE_TYPE_KERNEL 0x7
+
 typedef struct file_operations {
     int (*read) (file_node_t*, char *, size_t, size_t);
     int (*write) (file_node_t*, char**, size_t, size_t);
@@ -22,6 +30,8 @@ typedef struct file_operations {
     bool (*create)(file_node_t*, char*, int);
     int (*ioctl) (file_node_t*, unsigned long, void*);
 };
+
+typedef file_node_t* (*mount_func)(char*);
 
 typedef struct FILE {
     char name[256];
@@ -46,9 +56,12 @@ FILE* OpenFile(char* path);
 FILE* OpenStdIn();
 FILE* OpenStdOut();
 
+//File functions
 unsigned long open(char* filename, int mode);
 unsigned long create(char* filename, int mode);
 unsigned long mkdir(char* filename);
+
+//File descriptor functions
 void close(FILE* file);
 int seek(FILE* file, size_t len);
 int write(FILE* file, char* bytes, size_t len);
@@ -58,6 +71,12 @@ int get_size(FILE* file);
 int chmod(FILE* file, int mode);
 int chown(FILE* file, unsigned int user, unsigned int group);
 int ioctl(FILE* file, unsigned long request, void* args);
+int fcntl(FILE* file);
 list_dir_t* find(char* filename);
+
+//Mount functions
+void register_mount(char* name, mount_func func);
+void mount_directly(char* name, file_node_t root);
+void mount_empty(char* name, int fileType);
 
 #endif //NIGHTOS_VFS_H
