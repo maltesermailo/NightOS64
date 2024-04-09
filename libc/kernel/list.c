efe2ce69843b6297a15f7347c5b1fe1906d159d4
maltesermailo <jannik@andreasmueller.de>
2024-04-09 17:57:54 +0200

    Add some list functions

--- libc/kernel/list.c
+++ libc/kernel/list.c
@@ -3,13 +3,145 @@
 //
 #include "../include/kernel/list.h"
 #include "../../kernel/alloc.h"
+#include "../include/sys/cdefs.h"
 
 list_t * list_create() {
-    list_t* list = malloc(sizeof(list_t));
+    list_t* list = calloc(1, sizeof(list_t));
 
     list->length = 0;
     list->head = NULL;
     list->tail = NULL;
 
     return list;
+}
+
+void list_destroy(list_t* list) {
+    list_entry_t* listEntry = list->head;
+    list->head = NULL;
+
+    while(listEntry) {
+        list_entry_t* listEntryOld = listEntry;
+        free(listEntry->value);
+
+        listEntry = listEntry->next;
+
+        free(listEntryOld);
+    }
+
+    free(list);
+}
+
+void list_append(list_t* list, list_entry_t* entry) {
+    if(!list->length) {
+        list->head = entry;
+        list->tail = entry;
+        list->length = 1;
+
+        return;
+    }
+
+    list->tail->next = entry;
+    entry->prev = list->tail;
+    list->tail = entry;
+
+    list->length++;
+}
+
+void list_insert(list_t* list, void* item) {
+    list_entry_t * entry = calloc(1, sizeof(list_entry_t));
+    entry->value = item;
+
+    list_append(list, entry);
+}
+
+list_entry_t* list_find(list_t* list, void* item) {
+    if(!list->length) {
+        //List empty
+        return 0;
+    }
+
+    list_entry_t* entry = list->head;
+
+    for(;entry != 0;) {
+        if(entry->value == item) {
+            return entry;
+        }
+
+        entry = entry->next;
+    }
+
+    //No entry
+    return 0;
+}
+
+void list_remove_by_index(list_t* list, int index) {
+    if(!list->length) {
+        return;
+    }
+
+    if(index == list->length) {
+        list_delete(list, list->tail);
+    }
+
+    list_entry_t* entry = list->head;
+
+    if(index == 0) {
+        list_delete(list, entry);
+    } else {
+        for(int i = 1; i < index; i++) {
+            if(!entry->next) {
+                return;
+            }
+
+            entry = entry->next;
+        }
+
+        list_delete(list, entry);
+    }
+}
+
+void list_delete(list_t* list, list_entry_t* entry) {
+    list_entry_t* prev = entry->prev;
+    list_entry_t* next = entry->next;
+
+    if(list->head == entry) {
+        list->head = next; //If list size is 1, next will be 0 anyways
+    }
+
+    if(list->tail == entry) {
+        list->tail = prev; //Same as above
+    }
+
+    if(next && prev) {
+        //if both exist, link together
+        next->prev = prev;
+        prev->next = next;
+    } else {
+        //if one exists, remove their link to this entry
+        if(next) {
+            next->prev = 0;
+        }
+
+        if(prev) {
+            prev->next = 0;
+        }
+    }
+
+    entry->next = 0;
+    entry->prev = 0;
+
+    list->length--;
+}
+
+list_t* list_copy(list_t* original) {
+    list_t* new = calloc(1, sizeof(list_t));
+
+    for(list_entry_t* entry = original->head; entry != NULL; entry = entry->next) {
+        list_entry_t* copy = calloc(1, sizeof(list_entry_t));
+        copy->value = entry->value;
+
+        list_append(new, copy);
+    }
+
+    return new;
 }
