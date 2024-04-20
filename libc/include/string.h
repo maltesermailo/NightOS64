27a39d549038d45f3b6751a2e287beacb7aa48b2
maltesermailo <jannik@andreasmueller.de>
2024-04-20 19:18:51 +0200

    Add full string.h definitions

--- libc/include/string.h
+++ libc/include/string.h
@@ -9,13 +9,28 @@
 extern "C" {
 #endif
 
-int memcmp(const void*, const void*, size_t);
-void* memcpy(void* __restrict, const void* __restrict, size_t);
-void* memmove(void*, const void*, size_t);
-void* memset(void*, int, size_t);
-void* memchr(const void*, int c, size_t);
-size_t strlen(const char*);
-char* strdup(const char*);
+extern int memcmp(const void*, const void*, size_t);
+extern void* memcpy(void* __restrict, const void* __restrict, size_t);
+extern void* memmove(void*, const void*, size_t);
+extern void* memset(void*, int, size_t);
+extern void* memchr(const void*, int c, size_t);
+extern size_t strlen(const char*);
+extern char* strdup(const char*);
+extern char *strcpy(char *, const char *);
+extern char *strncpy(char *, const char *, size_t);
+
+extern char *strcat(char *, const char *);
+extern char *strncat(char *, const char *, size_t);
+extern int strcmp(const char *, const char *);
+extern int strcoll(const char *, const char *);
+extern int strncmp(const char *, const char *, size_t);
+extern char *strchr(const char *, int);
+extern size_t strcspn(const char *, const char *);
+extern char *strpbrk(const char *, const char *);
+extern char *strrchr(const char *, int);
+extern size_t strspn(const char *, const char *);
+extern char *strstr(const char *, const char *);
+extern char *strtok(char *, const char *);
 
 #ifdef __cplusplus
 }
