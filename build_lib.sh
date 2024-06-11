6ca070e8df9dbcf7b43277cb7165b6ade69b91e0
maltesermailo <jannik@andreasmueller.de>
2024-06-11 21:52:03 +0200

    Change lib build script

--- build_lib.sh
+++ build_lib.sh
@@ -1,5 +1,12 @@
 set -e
 . ./config.sh
 cd ./libc/
+
+export PREFIX=/usr/local/cross
+export EXEC_PREFIX=$PREFIX
+export BOOTDIR=/boot
+export LIBDIR=$EXEC_PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
 make clean
 make install
