7cfc51cb158ed627c926410c19f429580a25b8d5
maltesermailo <jannik@andreasmueller.de>
2024-06-12 16:54:31 +0200

    Change kernel build script for github

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 set -e
 . ./config.sh
+# Build script for GitHub Actions
+
+export PREFIX=/usr/local/cross/
+export EXEC_PREFIX=$PREFIX
+export BOOTDIR=/boot
+export LIBDIR=$EXEC_PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
 
 make build
