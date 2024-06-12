1bad288d778594213b3e0b5268625fde2da63e8e
maltesermailo <jannik@andreasmueller.de>
2024-06-12 16:57:37 +0200

    Rework of build scripts

--- build_lib.sh
+++ build_lib.sh
@@ -2,11 +2,5 @@ set -e
 . ./config.sh
 cd ./libc/
 
-export PREFIX=/usr/local/cross
-export EXEC_PREFIX=$PREFIX
-export BOOTDIR=/boot
-export LIBDIR=$EXEC_PREFIX/lib
-export INCLUDEDIR=$PREFIX/include
-
 make clean
 make install
