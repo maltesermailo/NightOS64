3bfbed7f4ce381ecd4cf5ef3eb600c8cd78896e6
maltesermailo <jannik@andreasmueller.de>
2024-06-14 23:29:56 +0200

    I forgot to add build.sh again

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ export AR=${HOST}-ar
 export AS=${HOST}-as
 export CC=${HOST}-gcc
  
-export PREFIX=/usr/local
+export PREFIX=${CROSS_PATH:-/usr/}
 export EXEC_PREFIX=$PREFIX
 export BOOTDIR=/boot
 export LIBDIR=$EXEC_PREFIX/lib
