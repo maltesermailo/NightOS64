11213a74fae9ca1983352eb54331c9586c0830fc
maltesermailo <jannik@andreasmueller.de>
2024-06-21 22:24:19 +0200

    Add init program with empty contents and prepare for hosted compiler

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ export AR=${HOST}-ar
 export AS=${HOST}-as
 export CC=${HOST}-gcc
 
-export PREFIX=${CROSS_PATH:-$HOME/opt/cross/x86_64-elf/}
+export PREFIX=${CROSS_PATH:-$HOME/opt/cross/x86_64-nightos/}
 export EXEC_PREFIX=$PREFIX
 export BOOTDIR=/boot
 export LIBDIR=$EXEC_PREFIX/lib
---
+++ init/init.c
@@ -0,0 +1,9 @@
+//
+// Created by Jannik on 21.06.2024.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+}
