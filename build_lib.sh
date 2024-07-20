9c82e2545bbbd72ab8b5291c13d8d68472cc157e
maltesermailo <jannik@andreasmueller.de>
2024-07-20 20:34:05 +0200

    Change patches

--- build_lib.sh
+++ build_lib.sh
@@ -4,3 +4,18 @@ cd ./libc/
 
 make clean
 make install
+
+cd ../mlibc/
+#Build mlibc
+meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=true build
+meson build
+cd ./build/
+ninja
+ninja install
+
+cd ../
+meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=false -Ddefault_library=shared build
+meson build
+cd ./build/
+ninja
+ninja install
