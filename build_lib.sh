7eee03366d8a699e13b9dfbaf6e06aa01c2bf55d
maltesermailo <jannik@andreasmueller.de>
2024-07-22 13:25:26 +0200

    Change to LF separator

--- build_lib.sh
+++ build_lib.sh
@@ -2,20 +2,20 @@ set -e
 . ./config.sh
 cd ./libc/
 
-make clean
-make install
+#make clean
+#make install-libs
 
 cd ../mlibc/
 #Build mlibc
-meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=true build
-meson build
-cd ./build/
-ninja
-ninja install
+#meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=true build
+#meson build
+#cd ./build/
+#ninja
+#ninja install
 
-cd ../
-meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=false -Ddefault_library=shared build
+#cd ../
+meson setup --wipe --cross-file ../nightos-meson-target.txt --prefix="$PREFIX" -Dheaders_only=false -Ddefault_library=both build
 meson build
 cd ./build/
-ninja
+ninja -v
 ninja install
